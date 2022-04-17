8ad5dc2d652c620670f75648973f2e544092c388
unknown <deeni1299@gmail.com>
2022-04-17 20:00:51 +0800

    init

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 require('babel-register');
 require('babel-polyfill');
 
@@ -5,10 +8,23 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
   },
+  
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
