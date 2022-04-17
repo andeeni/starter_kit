56ff0b437b93df07e7a50f4c2c3b24b87e917976
huichenglim <73864066+huichenglim@users.noreply.github.com>
2022-04-18 00:13:56 +0800

    Update Main.js
    changed # to product ID

--- src/components/Main.js
+++ src/components/Main.js
@@ -37,7 +37,7 @@ class Main extends Component {
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
+              <th scope="col">Product ID</th>
               <th scope="col">Name</th>
               <th scope="col">Price</th>
               <th scope="col">Owner</th>
